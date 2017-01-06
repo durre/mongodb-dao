3458181b8646513bdb2bb822df1d39c03d1e785e
Andreas Du Rietz <durred@gmail.com>
2017-01-06 22:40:15 +0100

    Breaking change using the new future based connection api

--- src/main/scala/se/durre/mongodb/MongoContext.scala
+++ src/main/scala/se/durre/mongodb/MongoContext.scala
@@ -3,14 +3,14 @@ package se.durre.mongodb
 import com.typesafe.config.Config
 import reactivemongo.api._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 object MongoContext {
 
   private lazy val driver = new MongoDriver
 
-  def connect(config: Config)(implicit ec: ExecutionContext): DefaultDB = {
+  def connect(config: Config)(implicit ec: ExecutionContext): Future[DefaultDB] = {
 
     val uri = config.getString("uri")
 
@@ -19,7 +19,7 @@ object MongoContext {
     ) { parsedUri =>
       val failoverStrategy = FailoverStrategy(retries = 10, initialDelay = 1.second)
       val connection = driver.connection(parsedUri)
-      connection(parsedUri.db.get, failoverStrategy)
+      connection.database(parsedUri.db.get, failoverStrategy)
     }
   }
 }
